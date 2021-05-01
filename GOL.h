1390d6877e368e3e708a77e94000f21be607ff7f
zhaoyanbo <sc19y3z@leeds.ac.uk>
2021-05-01 22:35:04 +0800

    Creat the head file of GOL

---
+++ GOL.h
@@ -0,0 +1,13 @@
+//
+// Created by coral on 2021/5/1.
+//
+
+#ifndef COURSEWORK2_GOL_H
+#define COURSEWORK2_GOL_H
+int selectGame(); //
+int selectStep(); //
+void init();
+void input();
+void display();
+void loadinit(FILE *file);
+#endif //COURSEWORK2_GOL_H
